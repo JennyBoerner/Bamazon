e93a2eb5f21a69e13c2e008aad505880ecbd2f28
Jenny Boerner <jennyboerner1@gmail.com>
2018-12-29 18:17:41 -0600

    Upload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+require("console.table");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -12,19 +13,13 @@ connection.connect(function(err) {
     if (err) throw err;
     console.log("Connected as ID " + connection.threadId);
     queryProducts();
-    questions();
-    
-    // connection.end();
 });
 
 function queryProducts() {
     connection.query("SELECT * FROM products", function(err, res) {
-        for (var i = 0; i < res.length; i++) {
-            if (res[i].stock_quantity > 0) {
-                console.log("ID: " + res[i].item_ID + " | " + res[i].product_name + " | $" + res[i].price + " | " + res[i].stock_quantity);  
-            }
-        }
-    });
+        console.table(res);
+        questions();
+    })
 }
 
 function questions() {
@@ -42,22 +37,26 @@ function questions() {
             }
         ])
         .then(function(answer) {
-            connection.query(
-                // var userQuantity = stock_quantity - answer.quantity;
-                "UPDATE products SET ? WHERE ?",
-                [
-                    {
-                        stock_quantity: answer.quantity
-                    },
-                    {
-                        item_ID: answer.item
-                    }
-                ],
-                function(error) {
-                    // if (err) throw err;
-                    console.log("Order Placed!");
+            var quantity = parseInt(answer.quantity);
+            var item = answer.item;
+            connection.query("SELECT * FROM products WHERE item_ID = ?", item, function(err, res) {
+                if (quantity > res[0].stock_quantity) {
+                    console.log("Not enough stock");
+                } else {
+                    orderQuantity(quantity, item);
+                    var price = quantity * res[0].price;
+                    console.log("Total Price: $" + Math.floor(price * 100) / 100);
                 }
-            );
+            })
         })
+}
+
+function orderQuantity(quantity, item) {
+    connection.query(
+        "UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_ID = ?",
+        [quantity, item],
+        console.log("Order Placed!")
+        
+    );
 }
   
